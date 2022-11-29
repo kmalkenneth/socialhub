28b12f0f96db5c5ab16772686dbe1f6e69ed03cc
Kenet Acuña <kmal.kenneth@gmail.com>
2022-11-28 23:41:22 -0600

    Add revoke mastodon

--- app/Http/Livewire/SocialNetworks.php
+++ app/Http/Livewire/SocialNetworks.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Services\MastodonApi;
 use App\Services\TwitterApi;
 use Livewire\Component;
 
@@ -14,21 +15,36 @@ class SocialNetworks extends Component
     public $twitterImgUri;
     public $twitterUsername;
 
-    public function mount(TwitterApi $api)
+    public $mastodonName;
+    public $mastodonImgUri;
+    public $mastodonUsername;
+
+    public function mount(TwitterApi $api, MastodonApi $mApi)
     {
         $twitterUser = $api->userMe();
+        $MUser = $mApi->userMe();
 
         if ($twitterUser) {
             $this->twitterName = $twitterUser->name;
             $this->twitterImgUri = $twitterUser->profile_image_url;
             $this->twitterUsername = $twitterUser->username;
         }
+
+        if ($MUser) {
+            $this->mastodonName = $MUser->display_name;
+            $this->mastodonImgUri = $MUser->avatar;
+            $this->mastodonUsername = $MUser->username;
+        }
     }
 
 
 
     public function twitterLogin(TwitterApi $api)
+    {
+        return redirect()->to($api->oauthUri);
+    }
 
+    public function mastodonLogin(MastodonApi $api)
     {
         return redirect()->to($api->oauthUri);
     }
